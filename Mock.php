226d72f485ee667d675fe1e1f1edd1cbb7fe61be
indeep-xyz <indeep.xyz@gmail.com>
2016-12-07 22:19:07 +0900

    Add the class Mock

---
+++ Mock.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MyLocalLogger;
+
+/**
+ * This class is a mock of an instance of Logger.
+ *
+ * @author  indeep-xyz
+ * @package MyLocalLoggers
+ * @version 0.1.0
+ */
+class Mock {
+  public function debug() {}
+  public function info() {}
+  public function warn() {}
+  public function error() {}
+  public function fatal() {}
+}
